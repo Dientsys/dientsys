b49e5559a922e2d85b932c49096e6df359a2be21
Erik Sandoval <eriksandoval.py@gmail.com>
2023-09-04 13:41:02 -0500

    Added user route.

--- routes/user.js
+++ routes/user.js
@@ -18,7 +18,20 @@ router.post('/post', async (req, res) => {
         if (checkUser) {
             return res.status(409).json({
                 message: 'User already exists.',
+            });
+        }
+        const user = new User({ name, password });
+        await user.save();
+
+        return res.status(201).json({
+            message: 'User createed successfully!',
+            user,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'INTERNAL SERVER ERROR',
         });
 
-        
     }
+});
